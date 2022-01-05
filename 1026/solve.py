016ec70a4b1500dc69cc7a90da1ffb3cc306de6b
조민호 <chomh168@hanmail.net>
2022-01-05 23:12:20 +0900

    정답

--- 1026/solve.py
+++ 1026/solve.py
@@ -1,1 +1,14 @@
-test = 1
+N = input()
+A = str(input()).split(' ')
+B = str(input()).split(' ')
+A = [ int(x) for x in A ]
+B = [ int(x) for x in B ]
+
+A.sort()
+B.sort(reverse=True)
+
+C = 0
+for index in range(int(N)):
+    C += (A[index] * B[index])
+
+print(C)
