af30096ab99c1bf3ed94eac1c58502f47bc00111
조민호 <chomh168@hanmail.net>
2022-01-11 21:40:55 +0900

    리팩토링

--- p_완전탐색_1/solve.py
+++ p_완전탐색_1/solve.py
@@ -3,32 +3,28 @@ secondStudent = [2, 1, 2, 3, 2, 4, 2, 5]
 thirdStudent  = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
 def solve(param):
-    one = [1,0]
-    two = [2,0]
-    thr = [3,0]
-    
-    count = 0
-    for each in param:
-        if each == firstStudent[count%len(firstStudent)]:
-            one[1] += 1
-        if each == secondStudent[count%len(secondStudent)]:
-            two[1] += 1
-        if each == thirdStudent[count%len(thirdStudent)]:
-            thr[1] += 1
-        count += 1
-    
-    compare = [one,two,thr]
-    compare.sort(key=lambda x:(-x[1],x[0]))
-
     result = []
-    max = compare[0][1]
-    for each in compare:
-        if each[1] == max:
-            result.append(each[0])
+    score = [0,0,0]
+
+    for count, each in enumerate(param):
+        if each == firstStudent[count%len(firstStudent)]:
+            score[0] += 1
+        if each == secondStudent[count%len(secondStudent)]:
+            score[1] += 1
+        if each == thirdStudent[count%len(thirdStudent)]:
+            score[2] += 1
+
+    for count, each in enumerate(score):
+        if each == max(score):
+            result.append(count+1)
 
     return result
 
 
 def solution(answers):
-    answer = []
     return solve(answers)
+
+
+answers = [1,3,2,4,2]#	[1]
+
+print(solution(answers))
