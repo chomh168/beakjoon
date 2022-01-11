c8944b41003aaf9c4d847148ab566dc78cb0411b
조민호 <chomh168@hanmail.net>
2022-01-11 21:35:50 +0900

    통과

---
+++ p_완전탐색_1/solve.py
@@ -0,0 +1,41 @@
+firstStudent  = [1, 2, 3, 4, 5]
+secondStudent = [2, 1, 2, 3, 2, 4, 2, 5]
+thirdStudent  = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+def solve(param):
+    one = [1,0]
+    two = [2,0]
+    thr = [3,0]
+    
+    count = 0
+    for each in param:
+        if each == firstStudent[count%len(firstStudent)]:
+            one[1] += 1
+        if each == secondStudent[count%len(secondStudent)]:
+            two[1] += 1
+        if each == thirdStudent[count%len(thirdStudent)]:
+            thr[1] += 1
+        count += 1
+    
+    compare = [one,two,thr]
+    compare.sort(key=lambda x:(-x[1],x[0]))
+    # print(compare)
+
+    result = []
+    max = compare[0][1]
+    for each in compare:
+        if each[1] == max:
+            result.append(each[0])
+
+    return result
+
+
+def solution(answers):
+    answer = []
+    return solve(answers)
+
+# answers = [1,2,3,4,5] #	[1]
+# # answers = [1,3,2,4,2] #	[1,2,3]
+answers = [3, 3, 2, 1, 5] # [3]
+
+print(solution(answers))
