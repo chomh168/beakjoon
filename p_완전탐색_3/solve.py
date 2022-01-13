d2d89ef69adeb9c10a3de566170ae81b44c578e2
조민호 <chomh168@hanmail.net>
2022-01-13 23:40:04 +0900

    코드 수정 및 다른 풀이 추가

--- p_완전탐색_3/solve.py
+++ p_완전탐색_3/solve.py
@@ -1,13 +1,6 @@
-# 10	2	[4, 3]
-# 8	1	[3, 3]
-brown = 10
-yellow = 2
-
-
-
 def makeRectByYellow(yellow):
     yellowRect = []
-    for each in range(1, yellow//2+2):
+    for each in range(1, int(yellow**0.5)+1):
         if yellow % each == 0:
             yellowRect.append([max(each, int(yellow/each)),min(each, int(yellow/each))])
     return yellowRect
@@ -17,14 +10,14 @@ def makeRectByBrown(yellowRect, brown):
         if (each[0] * 2 + each[1] * 2 + 4) == brown:
             return [each[0] + 2, each[1] + 2]
 
-# yellowRect = makeRectByYellow(yellow)
-# print(yellowRect)
-# print(makeRectByBrown(yellowRect, brown))
-
 def solution(brown, yellow):
     yellowRect = makeRectByYellow(yellow)
-    # print(yellowRect)
     answer = makeRectByBrown(yellowRect, brown)
     return answer
 
-print(solution(brown, yellow))
+# 수학적 사고 => 넓이를 통한 접근
+# def solution(brown, yellow):
+#     mn = brown + yellow
+#     for m in range(3, int(mn ** 0.5) + 1):
+#         if mn % m == 0 and 2 * (mn // m + m - 2) == brown:
+#             return [mn // m, m]
