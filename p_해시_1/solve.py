cb4006f093359d95284020977df183e5969f4746
조민호 <chomh168@hanmail.net>
2022-01-18 10:11:32 +0900

    통과

---
+++ p_해시_1/solve.py
@@ -0,0 +1,24 @@
+participant = ["leo", "kiki", "eden"]	
+completion = ["eden", "kiki"]
+
+def solution(participant, completion):
+    p = check(participant)
+    c = check(completion)
+    for name in c:
+        if p.get(name):
+            if c.get(name) == p.get(name):
+                p.pop(name)
+    for name in p:
+        answer = name
+    return answer
+
+def check(checkList):
+    result = {}
+    for each in checkList:
+        if result.get(each):
+            result[each] = result.get(each) + 1
+        else:
+            result[each] = 1
+    return result
+
+print(solution(participant, completion))
