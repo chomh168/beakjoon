22815823d61389cafa2e4947ef9f16595cb89e6f
조민호 <chomh168@hanmail.net>
2022-02-07 14:44:00 +0900

    정리

--- programmers/p_스택큐_4/solve.py
+++ programmers/p_스택큐_4/solve.py
@@ -1,12 +1,5 @@
 prices = [1, 2, 3, 2, 3]	
 # [4, 3, 1, 1, 0]
-# prices = [5, 4, 5, 2, 7]
-# prices = [1, 2, 3, 2, 3, 1]
-
-#           5, 4, 1, 2, 1, 0
-# 5 4 1 2 1 0 
-prices = [1, 2, 3, 2, 3, 3, 1]
-#[6, 5, 1, 3, 2, 1, 0]
 
 def solution(prices):
     answer = [0] * len(prices)
@@ -20,14 +13,3 @@ def solution(prices):
     
 print(prices)
 print(solution(prices))
-
-
-# def solution(prices):
-#     answer = [0] * len(prices)
-#     for i in range(len(prices)):
-#         for j in range(i+1, len(prices)):
-#             answer[i] += 1          
-#             if prices[j] < prices[i]:
-#                 break
-
-#     return answer
