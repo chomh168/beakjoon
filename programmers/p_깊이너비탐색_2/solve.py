5d9415a158496bf8dd3c38dfcb368650559d2949
조민호 <chomh168@hanmail.net>
2022-02-16 22:34:38 +0900

    통과

--- programmers/p_깊이너비탐색_2/solve.py
+++ programmers/p_깊이너비탐색_2/solve.py
@@ -1,25 +1,36 @@
 # 3	[[1, 1, 0], [1, 1, 0], [0, 0, 1]]	2
 # 3	[[1, 1, 0], [1, 1, 1], [0, 1, 1]]	1
 
-n = 3
-computers = [[1, 1, 0], [1, 1, 1], [0, 1, 1]] # [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
 
-def connect(n, computers, case):
-    for i in range(1,n+1):
-        for j in range(i+1,n+1):
-            if computers[i-1][j-1] == 1:
-                case[i].extend(case[j])
-                case[j] = case[i]
-                case[0] -= 1
-    print(case)
-    return case[0]
+
+# n = 3
+# computers = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
+n = 4
+computers = [[1, 1, 0, 1], [1, 1, 0, 0], [0, 0, 1, 1], [1, 0, 1, 1]]
+
+from collections import deque
+
+def connect(n, computers):
+    visited = [False] * (n)
+    que = deque()
+    count = 0
+    for i in range(n):
+        if visited[i] == False:
+            count += 1
+            que.append(i)
+            while len(que) != 0:
+                element = que.popleft()
+                visited[element] = True
+                for idx, each in enumerate(computers[element]):
+                    if each == 1 and visited[idx] == False:
+                        print(element, idx)
+                        que.append(idx)
+    return count
+    
 
 
 def solution(n, computers):
-    case = [n]
-    for i in range(n):
-        case.append([i+1])
-    answer = connect(n, computers, case)
+    answer = connect(n, computers)
     return answer
 
 print(solution(n, computers))
