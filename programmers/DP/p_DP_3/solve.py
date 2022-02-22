f90c71d4da496ca0ba4285f87a94e77fb07a9709
조민호 <chomh168@hanmail.net>
2022-02-22 20:40:20 +0900

    통과

--- programmers/DP/p_DP_3/solve.py
+++ programmers/DP/p_DP_3/solve.py
@@ -3,9 +3,19 @@ n = 3
 puddles = [[2, 2]]#	4
 
 def solve(m, n, puddles):
-    DP = [ ]
-    pass
+    DP = [ [0] * (n+1) for _ in range(m+1)]
+
+    DP[1][1] = 1
+    for x in range(1,n+1):
+        for y in range(1,m+1):
+            if [y,x] in puddles or (x == 1 and y == 1):
+                continue
+            DP[y][x] = (DP[y-1][x] + DP[y][x-1]) % 1000000007
+    
+    return DP[m][n]
 
 def solution(m, n, puddles):
-    answer = 0
+    answer = solve(m, n, puddles)
     return answer
+
+print(solution(m, n, puddles))
